f6704b8cc79a4e32e3a3d49adb558079735f60e6
Ivan Perez <80877217+ipa314@users.noreply.github.com>
2021-03-28 02:08:39 -0400

    Update cmbprod.cpp

--- cmbprod.cpp
+++ cmbprod.cpp
@@ -1,9 +1,35 @@
 
+class meta{
+  //inputs: m;
+  //size of words=J;
+  public:
 
-input: m
+  void getmeta(const int m, const int J)
+  nrows=(pow(m+1,J+1)-1)/m-1;
+  ncols=J;
 
-array final=build array of size ((m+1)^(J+1)-1)/m-1, J columns 
-
-for each i in array 0:m
-  arrayfinal=
+  int allprod[nrows][ncols+1];
+  for (int i=1;i<J+1;i++){
+    sizeword=i;
+    accwrdssizeim1=(m+1)*(pow(m+1,sizeword-1)/m);
+    accwrdssizei=(m+1)*(pow(m+1,sizeword)/m);
+    startrowi=accwrdssizeim1;
+    endrowi1=accwrdssizei;
+    
+    for (int j=startrowi;j<endrowi1;j++){
+      for (int ccol=J;ccol>=J;ccol--){
+        arrmeta[j][ccol]=;
+        
+      }
+        
+        
+        
+      }
+    
+  for each i in array 0:m  // 
+    arrayfinal=
+  }
   
+
+  
+}
